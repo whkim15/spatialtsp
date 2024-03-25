1364db77f75e95e78e261c3e9e7ff2c12a7f9eb2
whkim15 <wkim15@vols.utk.edu>
2024-03-24 23:11:33 -0400

    1

--- spatialtsp/spatialtsp.py
+++ spatialtsp/spatialtsp.py
@@ -4,14 +4,6 @@
 import ipyleaflet
 from ipyleaflet import basemaps
 
-# For spatial_tsp
-import numpy as np
-import random
-from scipy.spatial import distance, Voronoi
-from sklearn.neighbors import NearestNeighbors
-import geopandas as gpd
-from shapely.geometry import Point
-
 class Map(ipyleaflet.Map):
     def __init__(self, center=[20, 0], zoom=2, **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)

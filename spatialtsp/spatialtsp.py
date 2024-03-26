8200f2356537e9421b4b7919eba48090a2a37fa9
whkim15 <wkim15@vols.utk.edu>
2024-03-26 18:52:13 -0400

    add docstring

--- spatialtsp/spatialtsp.py
+++ spatialtsp/spatialtsp.py
@@ -20,10 +20,21 @@ class Map(ipyleaflet.Map):
         super().__init__(center=center, zoom=zoom, **kwargs)
 
     def add_tile_layer(self, url, name, **kwargs):
+        """Add tile layer
+
+        Args:
+            url (str): The address of url.
+            name (str): The name of the layer. 
+        """
         layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
         self.add(layer)
 
     def add_basemap(self, name):
+        """Adds a basemap to the current map.
+
+        Args:
+            name (str or object): The name of the base map as a string or object
+        """
         if isinstance(name, str):
             url = eval(f"basemaps.{name}").build_url()
             self.add_tile_layer(url, name)
@@ -31,9 +42,20 @@ class Map(ipyleaflet.Map):
             self.add(name)
 
     def add_layers_control(self, position = "topright"):
+        """Adds a layers control to the map.
+
+        Args:
+            position (str, optional): The position of the layers control. Defaults to "topright".
+        """
         self.add_control(ipyleaflet.LayersControl(position=position))
 
     def add_geojson(self, data, name="geojson", **kwargs):
+        """Adds a GeoJSON layer to the map.
+
+        Args:
+            data (str | dict): GeoJSON data as a string or a dictionary.
+            name (str, optional): The name of the layer. Defaults to "geojson"
+        """
 
         import json
 
@@ -51,6 +73,12 @@ class Map(ipyleaflet.Map):
         self.add(layer)
 
     def add_shp(self, data, name='shp', **kwargs):
+        """Adds a shapefile to the map 
+
+        Args:
+            data (str or dict): The path to the map
+            name (str, optional): The name of the shapefile. Defaults to 'shp'.
+        """
         import shapefile
         import json
 

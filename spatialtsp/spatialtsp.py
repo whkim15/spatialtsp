c69d3a88a35f4cb4ed5fa42332bf5bdbe6bcbf5a
whkim15 <wkim15@vols.utk.edu>
2024-03-26 21:18:34 -0400

    add spatialtsp

--- spatialtsp/spatialtsp.py
+++ spatialtsp/spatialtsp.py
@@ -4,6 +4,14 @@
 import ipyleaflet
 from ipyleaflet import basemaps
 
+# For spatialtsp
+import numpy as np
+import random
+from scipy.spatial import distance, Voronoi
+from sklearn.neighbors import NearestNeighbors
+import geopandas as gpd
+from shapely.geometry import Point
+
 class Map(ipyleaflet.Map):
     """This is the map class that inherits from ipyleaflet.Map.
 
@@ -87,3 +95,57 @@ class Map(ipyleaflet.Map):
                 data = shp.__geo_interface__
         
         self.add_geojson(data, name, **kwargs)
+
+class tsp(spatialtsp.tsp):
+    def is_far_enough(new_point, existing_points, min_distance=3):
+        """Check if a new point is far enough from existing points.
+
+        Args:
+            new_point (any type of number data): Make a new point
+            existing_points (any type of number data): For get a enough distance from existing points
+            min_distance (int, optional): The minimum distance between points. Defaults to 3.
+
+        Returns:
+            _type_: _description_
+        """
+        for point in existing_points:
+            if np.sqrt((new_point[0] - point[0])**2 + (new_point[1] - point[1])**2) < min_distance:
+                return False
+        return True
+
+    def generate_clustered_points(num_points, std_dev=5, cluster_centers=[(13, 13), (37, 37)], x_max=50, y_max=50, min_distance=3, seed=None):
+        """Generate clustered points.
+
+        Args:
+            num_points (int): The number of points to generate.
+            std_dev (int, optional): make std dev. Defaults to 5.
+            cluster_centers (list, optional): The centers of the clusters. Defaults to [(13, 13), (37, 37)].
+            x_max (int, optional): Limitation of x value. Defaults to 50.
+            y_max (int, optional): Limitation of y value Defaults to 50.
+            min_distance (int, optional): The minimum distance between points. Defaults to 3.
+            seed (_type_, optional): Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        np.random.seed(seed)
+        all_points = set()
+
+        points_per_cluster = num_points // len(cluster_centers)
+        extra_points = num_points % len(cluster_centers)  #    
+        
+        for index, center in enumerate(cluster_centers):
+            points = set()
+            extra = 1 if index < extra_points else 0
+            while len(points) < points_per_cluster + extra:
+                x = np.random.normal(center[0], std_dev)
+                y = np.random.normal(center[1], std_dev)
+                x, y = int(round(x)), int(round(y))
+                if x > 1 and y > 1 and x <= x_max and y <= y_max and is_far_enough((x, y), all_points, min_distance):
+                    points.add((x, y))
+            all_points.update(points)
+        
+        points_list = list(all_points)
+        gdf_points = gpd.GeoDataFrame({'geometry': [Point(p) for p in points_list]}, crs="EPSG:4326")
+        
+        return gdf_points

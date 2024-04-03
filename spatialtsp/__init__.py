dd371fa91d32a29a4f9690295541f94998b276ad
whkim15 <wkim15@vols.utk.edu>
2024-04-03 01:33:34 -0400

    version

--- spatialtsp/__init__.py
+++ spatialtsp/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Wanhee Kim"""
 __email__ = "wkim15@vols.utk.edu"
-__version__ = "0.0.2"
+__version__ = "0.0.4"
 
 from .spatialtsp import Map, is_far_enough, generate_clustered_points, calculate_distance_matrix
